8ac9dd731ef18053457cd6dd41ccf5684eb93029
john <john123@gmail.com>
2024-02-16 10:10:45 +0530

    new

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abcd
+{
+    public class Class1
+    {
+        public void add(double n1,double n2)
+        {
+            Console.WriteLine("Result add : "+(n1+n2));
+        }
+        public void sub(double n1, double n2)
+        {
+            Console.WriteLine("Result sub: " + (n1 - n2));
+        }
+        public void mul(double n1, double n2)
+        {
+            Console.WriteLine("Result mul : " + (n1 * n2));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using abcd;
+Class1 class1 = new Class1();
+
+Console.WriteLine("Hello, World!");
+class1.add(5, 5);
